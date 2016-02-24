9c2922d080c5cb959f090bf60236e0c0b28d3b6f
Cayla Scarrow <yaas.cs21@hotmail.com>
2016-02-24 09:34:49 -0500

    Made list to draw the star

--- DrawStar/Form1.cs
+++ DrawStar/Form1.cs
@@ -28,28 +28,35 @@ namespace DrawStar
         /// <param name="x">where to start drawing star on the x axis</param>
         /// <param name="y">where to start drawing star on the y axis</param>
         /// <param name="finalSize"> the size of star drawn</param>
-        public void DrawStar(Graphics g, Pen pen, float x, float y, float finalSize)
+        public void DrawStar(Graphics g, Pen pen, int x, int y, int finalSize)
         {
-            g.DrawLine(pen, (finalSize * (0 + x)) / 205, (finalSize * (75 + y)) / 195, (finalSize * (79 + x)) / 205, (finalSize * (75 + y)) / 195);
-            g.DrawLine(pen, (finalSize * (79 + x)) / 205, (finalSize * (75 + y)) / 195, (finalSize * (103 + x)) / 205, (finalSize * (0 + y)) / 195);
-            g.DrawLine(pen, (finalSize * (103 + x)) / 205, (finalSize * (0 + y)) / 195, (finalSize * (127 + x)) / 205, (finalSize * (75 + y)) / 195);
-            g.DrawLine(pen, (finalSize * (127 + x)) / 205, (finalSize * (75 + y)) / 195, (finalSize * (205 + x)) / 205, (finalSize * (75 + y)) / 195);
-            g.DrawLine(pen, (finalSize * (205 + x)) / 205, (finalSize * (75 + y)) / 195, (finalSize * (141 + x)) / 205, (finalSize * (122 + y)) / 195);
-            g.DrawLine(pen, (finalSize * (141 + x)) / 205, (finalSize * (122 + y)) / 195, (finalSize * (165 + x)) / 205, (finalSize * (195 + y)) / 195);
-            g.DrawLine(pen, (finalSize * (165 + x)) / 205, (finalSize * (195 + y)) / 195, (finalSize * (103 + x)) / 205, (finalSize * (149 + y)) / 195);
-            g.DrawLine(pen, (finalSize * (103 + x)) / 205, (finalSize * (149 + y)) / 195, (finalSize * (39 + x)) / 205, (finalSize * (195 + y)) / 195);
-            g.DrawLine(pen, (finalSize * (39 + x)) / 205, (finalSize * (195 + y)) / 195, (finalSize * (64 + x)) / 205, (finalSize * (122 + y)) / 195);
-            g.DrawLine(pen, (finalSize * (64 + x)) / 205, (finalSize * (122 + y)) / 195, (finalSize * (0 + x)) / 205, (finalSize * (75 + y)) / 195);
+            Point point1 = new Point((finalSize * (0 + x)) / 205, (finalSize * (75 + y)) / 195);
+            Point point2 = new Point((finalSize * (79 + x)) / 205, (finalSize * (75 + y)) / 195);
+            Point point3 = new Point((finalSize * (103 + x)) / 205, (finalSize * (0 + y)) / 195);
+            Point point4 = new Point((finalSize * (127 + x)) / 205, (finalSize * (75 + y)) / 195);
+            Point point5 = new Point((finalSize * (205 + x)) / 205, (finalSize * (75 + y)) / 195);
+            Point point6 = new Point((finalSize * (141 + x)) / 205, (finalSize * (122 + y)) / 195);
+            Point point7 = new Point((finalSize * (165 + x)) / 205, (finalSize * (195 + y)) / 195);
+            Point point8 = new Point((finalSize * (103 + x)) / 205, (finalSize * (149 + y)) / 195);
+            Point point9 = new Point((finalSize * (39 + x)) / 205, (finalSize * (195 + y)) / 195);
+            Point point10 = new Point((finalSize * (64 + x)) / 205, (finalSize * (122 + y)) / 195);
+
+            Point[] star = { point1, point2, point3, point4, point5, point6, point7, point8, point9, point10 };
+
+            g.DrawPolygon(pen, star);
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            float size = 75;
-            float yValue = 10;
-            float xValue = 70;
+            int xValue = 70;
+            int size = 75;
+            int yValue = 10;
+            
             Graphics g = this.CreateGraphics();
             Pen pen = new Pen(Color.Black, 1);
             DrawStar(g, pen, xValue, yValue, size);
+
+            
         }
     }
 }
